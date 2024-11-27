0e75753bcea8ec91662d0c50848da3b78b24274f
farrasyakila <farrasyakila@gmail.com>
2024-11-27 10:26:01 +0700

    update docker-compose

---
+++ code/init.sql
@@ -0,0 +1,7 @@
+USE nodejs_api; 
+CREATE TABLE IF NOT EXISTS `person` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`first_name` varchar(50) NOT NULL DEFAULT '0',
+	`last_name` varchar(50) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
+);
